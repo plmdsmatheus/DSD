60ded7949a9e816154ccd762f8f294e6fc37b903
Matheus Duarte <84824460+plmdsmatheus@users.noreply.github.com>
2023-12-01 12:07:06 +0000

    Gateway implementado - Rest

--- Rest/app/api_social/main.py
+++ Rest/app/api_social/main.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     import uvicorn
 
     # Iniciar o servidor com Uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="localhost", port=8000, reload=True)
